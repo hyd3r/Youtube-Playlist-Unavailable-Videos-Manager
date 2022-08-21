bc79a625373492ec6dab6db2bfc290554fefd9fb
Cris <42913116+hyd3r@users.noreply.github.com>
2022-08-22 02:34:01 +0800

    Prototype complete
    All initially planned functions are implemented

--- main.py
+++ main.py
@@ -8,11 +8,38 @@ import googleapiclient.errors
 import PySimpleGUI as sg
 from PIL import Image
 import requests
+import pyperclip
 
-scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+
+class Table(sg.Table):
+    def _RightClickMenuCallback(self, event):
+        self.user_event = event
+        self.TKRightClickMenu.tk_popup(event.x_root, event.y_root, 0)
+        self.TKRightClickMenu.grab_release()
+
+    def position(self, event):
+        region = self.Widget.identify('region', event.x, event.y)
+        if region == 'heading':
+            row = -1
+        elif region == 'cell':
+            row = int(self.Widget.identify_row(event.y)) - 1
+        elif region == 'separator':
+            row = None
+        else:
+            row = None
+        col_identified = self.Widget.identify_column(event.x)
+        if col_identified:
+            column = int(self.Widget.identify_column(event.x)[1:]) - 1 - int(self.DisplayRowNumbers is True)
+        else:
+            column = None
+        return row, column
+
+
+scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
 
 
 def main():
+
     layout1 = [[sg.Text('Please authenticate')],
                [sg.Button('Click here to Login')]]
 
@@ -23,7 +50,7 @@ def main():
         if event == sg.WIN_CLOSED:
             break
         elif event == 'Click here to Login':
-            os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+            os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "0"
 
             api_service_name = "youtube"
             api_version = "v3"
@@ -36,6 +63,7 @@ def main():
                 api_service_name, api_version, credentials=credentials)
 
             window.close()
+
 
     request = youtube.playlists().list(
         part="snippet,contentDetails",
@@ -55,7 +83,8 @@ def main():
         png_data = png_bio.getvalue()
 
         layout1[num] = [[sg.Image(data=png_data, key="-PL-IMG-")],
-                        [sg.Button(str(item["snippet"]["title"]), key=item["id"]+" "+str(item["contentDetails"]["itemCount"]))]]
+                        [sg.Button(str(item["snippet"]["title"]),
+                                   key=item["id"] + " " + str(item["contentDetails"]["itemCount"]))]]
         num += 1
 
     layout = [[sg.Column(layout1[ite], element_justification='c') for ite in range(num)]]
@@ -76,9 +105,10 @@ def main():
     window.close()
 
     layout = [[sg.Text("Checking the videos in the playlist. Please wait")],
-        [sg.ProgressBar(playlist_count, orientation='h', size=(50,20), border_width=4, key='-PROGRESS_BAR-', bar_color=("deep sky blue", "snow"))],
-              [sg.Text("0/"+playlist_count, key='-PROGRESS_TEXT-')]
-    ]
+              [sg.ProgressBar(playlist_count, orientation='h', size=(50, 20), border_width=4, key='-PROGRESS_BAR-',
+                              bar_color=("deep sky blue", "snow"))],
+              [sg.Text("0/" + playlist_count, key='-PROGRESS_TEXT-')]
+              ]
 
     window = sg.Window("Loading", layout, finalize=True)
     loading_progress = 0
@@ -91,6 +121,7 @@ def main():
     selected_playlist = request.execute()
     cont = True
     nextToken = ""
+    data = []
     while cont:
         if "nextPageToken" in selected_playlist:
             nextToken = selected_playlist["nextPageToken"]
@@ -108,15 +139,16 @@ def main():
             )
             res = request.execute()
             if res["pageInfo"]["totalResults"] == 0:
-                print(sp_item["snippet"]["title"] + " ==== " + sp_item["snippet"]["resourceId"][
-                    "videoId"] + " ==== " + sp_item["id"])
+                data.append([sp_item["snippet"]["title"], "https://www.youtube.com/watch?v=" + str(sp_item["snippet"]["resourceId"][
+                                    "videoId"]), sp_item["id"]])
             elif res["pageInfo"]["totalResults"] > 0:
                 if "regionRestriction" in res["items"][0]["contentDetails"]:
                     if "blocked" in res["items"][0]["contentDetails"]["regionRestriction"]:
                         for block in res["items"][0]["contentDetails"]["regionRestriction"]["blocked"]:
                             if block == "PH":
-                                print(sp_item["snippet"]["title"] + " ==== " + sp_item["snippet"]["resourceId"][
-                                    "videoId"] + " ==== " + sp_item["id"])
+                                data.append([sp_item["snippet"]["title"],
+                                             "https://www.youtube.com/watch?v=" + str(sp_item["snippet"]["resourceId"][
+                                                                                          "videoId"]), sp_item["id"]])
 
         request = youtube.playlistItems().list(
             part="snippet",
@@ -128,8 +160,50 @@ def main():
 
     window.close()
 
+    headings = ["Title", "Link", "Video ID"]
+    right_click_menu = ['&Right', ['Copy']]
 
+    layout = [
+        [Table(data, headings=headings, enable_events=True, auto_size_columns=True, display_row_numbers=True,
+               justification='center', right_click_menu=right_click_menu, key='-TABLE-')],
+        [sg.Button("Open playlist video removal window", key="del"),
+         sg.Text("You can right click any field to copy the text")]
+    ]
+    window = sg.Window("All unavailable videos in your playlist", layout, finalize=True, resizable=True)
+    table = window["-TABLE-"]
 
+    while True:
+        event, values = window.read()
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            break
+        elif event == "Copy":
+            e = table.user_event
+            row, col = position = table.position(e)
+            if None not in position and row >= 0:
+                text = data[row][col]
+                print(text)
+                pyperclip.copy(text)
+        elif event == "del":
+            layout2 = [
+                [sg.Text('Video ID'), sg.Input(key='_IN_')],
+                [sg.Button("Remove", key="rem"),
+                 sg.Text("Enter the Video ID of the video you wish to remove from your playlist", key="rem_status")]
+            ]
+            window = sg.Window("Remove video from playlist", layout2, finalize=True)
+            while True:
+                event, values = window.read()
+                if event == "Exit" or event == sg.WIN_CLOSED:
+                    break
+                elif event == "rem":
+                    window['rem_status'].update("Removing...")
+                    request = youtube.playlistItems().delete(
+                        id=values["_IN_"]
+                    )
+                    test = request.execute()
+                    window['rem_status'].update("Success!")
+
+            window.close()
+    window.close()
 
 
 if __name__ == "__main__":
